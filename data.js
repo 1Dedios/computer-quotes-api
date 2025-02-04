a87052ddc78255e5edcc2f64c1c0b87968ec01b9
Dedios <dediosprogramming@gmail.com>
2025-02-03 19:06:09 -0500

    fix: removed capital letter from person attr in data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const quotes = [
   {
     quote:
       'The best thing about a boolean is even if you are wrong, you are only off by a bit.',
-    person: 'Anonymous',
+    person: 'anonymous',
   },
   {
     quote: `If it's a good idea, go ahead and do it. It's much easier to apologize than it is to get permission.`,
